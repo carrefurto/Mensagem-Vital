1de63ee218d3f8a47692a0e71345656ac52aa9e7
carrefurto <mhuyfhafo@gmail.com>
2024-05-29 16:44:47 -0300

    MensagemVital.java

--- src/MensagemVital.java
+++ src/MensagemVital.java
@@ -3,20 +3,25 @@ import java.util.concurrent.ThreadLocalRandom;
 
 public class MensagemVital {
 
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) {
         Scanner leitor = new Scanner(System.in);
 
-        int dificuldadeEscolhida;
+        int dificuldadeEscolhida = 0;
         System.out.println("MensagemVital");
         System.out.println();
-        System.out.print("Qual a dificuldade? (4 - 10): ");
-
+        System.out.print("QUAL A DIFICULDADE (4 - 10): ");
         dificuldadeEscolhida = leitor.nextInt();
 
         while (dificuldadeEscolhida < 4 || dificuldadeEscolhida > 10) {
             System.out.println("Escolha entre 4 e 10: ");
             dificuldadeEscolhida = leitor.nextInt();
         }
+        System.out.println("ENVIE ESTA MENSAGEM");
+
+        geracaoDaPalavra(dificuldadeEscolhida);
+    }
+
+    private static void geracaoDaPalavra(int dificuldadeEscolhida) {
 
         ThreadLocalRandom geradorDePalavras = ThreadLocalRandom.current();
         // o ThreadL.R. ele é o "Random" só que para palavras
@@ -24,18 +29,44 @@ public class MensagemVital {
 
         for (int i = 1; i <= dificuldadeEscolhida; i++) {
             char palavra = (char) geradorDePalavras.nextInt(65, 91);
+            //casting é algo como int valor = 73; char letra (char) valor; temporariamente o int atua como um char, porém não é.
             mensagem.append(palavra);
-        } /*o i equivale a um número base para formar a palavra corretamente,
+             /*o i equivale a um número base para formar a palavra corretamente,
          se i for menor ou igual à dificuldade escolhida i soma mais 1, assim aleatorizando mais uma letra*/
-
+        }
         System.out.println(mensagem);
-
         sleep(dificuldadeEscolhida);
 
+        fimDoJogo(mensagem);
     }
 
-    private static void sleep (int dificuldadeEscolhida) throws InterruptedException {
-        int tempo = dificuldadeEscolhida *1000;
-        Thread.sleep(tempo);
+    private static void sleep(int dificuldadeEscolhida) {
+        int tempo = dificuldadeEscolhida * 1000;
+        int numeroDeLinhas = 2000;
+
+        try {
+            Thread.sleep(tempo);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
         }
+        for (int i = 0; i < numeroDeLinhas; i++) {
+            System.out.println();
+        }
+    }
+
+    private static void fimDoJogo(StringBuilder mensagem) {
+        Scanner leitorTxt = new Scanner(System.in);
+        String mensagemDigitada = "";
+
+        System.out.println("DIGITE A MENSAGEM AGORA!");
+        mensagemDigitada = leitorTxt.nextLine();
+
+        if (mensagemDigitada.equalsIgnoreCase(mensagem.toString())) {
+            System.out.println("VOCÊ SALVOU O MUNDO");
+        } else {
+            System.out.println("O MUNDO ACABOU, QUE PENA...");
+        }
+
+        System.exit(0);
+    }
 }
